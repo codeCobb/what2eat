4904eafed67397c4fd204be38ddde0370f9ce4d7
cobb <cobb@cobb.com>
2026-02-28 10:27:19 +0800

    增加菜品以及alembic数据库迁移功能

---
+++ src/dishes/model.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.core.base_model import Base, DateTimeMixin
+
+# if TYPE_CHECKING:
+#     from src.collections.model import Collection
+
+
+class Dish(Base, DateTimeMixin):
+    __tablename__ = "dishes"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    description: Mapped[str | None] = mapped_column(Text)
+
+    # collections: Mapped[list["Collection"]] = relationship(
+    #     "Collection",
+    #     secondary="collection_dish",
+    #     back_populates="dishes",
+    # )
--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@
 from fastapi import FastAPI, Response, Depends
 
 from src.core.config import settings
-
 from src.core.config import get_settings, Settings
+from src.lifespan import lifespan
+
+from src.core.exception import register_exception_handlers
+from src.dishes.router import router as dish_router
 
 # from dotenv import load_dotenv
 # from pathlib import Path
@@ -13,27 +16,31 @@ from src.core.config import get_settings, Settings
 app = FastAPI(
     app_name=settings.app_name,
     version="0.1.0",
-    description="FastAPI 练习项目实战"
+    description="FastAPI 练习项目实战",
+    lifespan=lifespan
 )
 
 
+register_exception_handlers(app)
+
+app.include_router(dish_router)
 
 # 路由引入
-@app.get("/")
-def read_root(
-    # 使用 FastAPI 的依赖注入系统来获取配置实例
-    # FastAPI 会自动调用 get_settings()，由于缓存的存在，这几乎没有开销
-    settings: Settings = Depends(get_settings),
-):
-    """
-    一个示例端点，演示如何访问配置。
-    """
-    return {
-        "message": f"Hello from the {settings.app_name}!",
-        # 演示如何使用在模型中动态计算的属性
-        "database_url": settings.database_url,
-        "jwt_secret": settings.jwt_secret,
-    }
+# @app.get("/")
+# def read_root(
+#     # 使用 FastAPI 的依赖注入系统来获取配置实例
+#     # FastAPI 会自动调用 get_settings()，由于缓存的存在，这几乎没有开销
+#     settings: Settings = Depends(get_settings),
+# ):
+#     """
+#     一个示例端点，演示如何访问配置。
+#     """
+#     return {
+#         "message": f"Hello from the {settings.app_name}!",
+#         # 演示如何使用在模型中动态计算的属性
+#         "database_url": settings.database_url,
+#         "jwt_secret": settings.jwt_secret,
+#     }
 
 
 @app.get("/health")
